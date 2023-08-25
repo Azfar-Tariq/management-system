46da223c2b586b3c219dc11052254f16f89a933c
Azfar-Tariq <azfaro2031@gmail.com>
2023-08-25 15:51:29 +0500

    Adjusted Some Things

--- src/components/MessageBox.jsx
+++ src/components/MessageBox.jsx
@@ -1,27 +1,22 @@
-import { IconButton, TextField, Tooltip } from "@mui/material";
+import { IconButton, InputAdornment, TextField, Tooltip } from "@mui/material";
 import React from "react";
 import EmojiEmotionsIcon from "@mui/icons-material/EmojiEmotions";
 
 function MessageBox() {
 	return (
 		<TextField
+			variant='outlined'
 			placeholder='Aa'
-			variant='filled'
 			fullWidth
 			InputProps={{
-				style: {
-					height: "40px",
-					padding: "0px",
-				},
-				inputstyle: {
-					textAlign: "center",
-				},
 				endAdornment: (
-					<Tooltip title='Emoji'>
-						<IconButton>
-							<EmojiEmotionsIcon />
-						</IconButton>
-					</Tooltip>
+					<InputAdornment position='end'>
+						<Tooltip title='Emoji'>
+							<IconButton>
+								<EmojiEmotionsIcon />
+							</IconButton>
+						</Tooltip>
+					</InputAdornment>
 				),
 			}}
 		/>
